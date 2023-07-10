7c63fbfe6bf82c8f30cfa29e2c95213da27327ec
v.a.konovalov <v.a.konovalov@fun-box.ru>
2023-07-10 19:17:31 +0300

    add restart

--- lib/game_of_stones/client.ex
+++ lib/game_of_stones/client.ex
@@ -16,7 +16,9 @@ defmodule GameOfStones.Client do
       {:next_turn, next_player, stones_count} ->
         IO.puts("\nХод игрока: #{next_player}. Осталось камней: #{stones_count}")
         next_turn()
-      {:winner, winner} -> IO.puts("\nИгрок: #{winner} победил.")
+      {:winner, winner} ->
+        IO.puts("\nИгрок: #{winner} победил.")
+        restart?()
       {:error, reason} ->
         IO.puts("\nПроизошла ошибка: #{reason}.")
         next_turn()
@@ -33,4 +35,32 @@ defmodule GameOfStones.Client do
 
   defp stones_to_take(:error), do: 0
   defp stones_to_take({count, _}), do: count
+
+  def restart?() do
+    IO.puts("\n Желаете начать игру заново?")
+    IO.gets("\n Введите цифру(0 - нет, 1 - да):") |>
+    String.trim() |>
+    Integer.parse() |>
+    restart?()
+  end
+
+  defp restart?({num, _}) when num == 1,  do: start()
+  defp restart?(_), do: IO.puts("До встречи.")
+
+  defp start do
+    get_count_stones() |> play()
+  end
+
+  defp get_count_stones do
+    IO.puts("\n Какое количество камней будет в куче ( не менее 3)?")
+    {count_stones, _} =
+      IO.gets("\n Введите цифру:") |>
+      String.trim() |>
+      Integer.parse()
+
+    cond do
+      count_stones >= 3 -> count_stones
+      count_stones < 3 -> get_count_stones()
+    end
+  end
 end
